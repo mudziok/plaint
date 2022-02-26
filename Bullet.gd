4181f7beab9c6dbf83ef9160bec4c6e6a79cd798
DroneMaker123 <59488688+DroneMaker123@users.noreply.github.com>
2022-02-26 13:05:15 +0100

    Add bullet, testbulletscene

---
+++ Bullet.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+export (NodePath) onready var player = get_node(player)
+
+var velocity = 350
+var rotate_speed = 2
+var target_direction = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = Vector2(300,300) #do wywalenia po testach
+	pass # Replace with function body.
+
+
+
+func _process(delta):
+
+	var target_vector = Vector2(player.position.x - position.x,player.position.y - position.y)
+	target_direction = target_vector.angle()+PI/2
+	
+	rotation = lerp_angle(rotation, target_direction, 0.05)
+	position += Vector2.UP.rotated(rotation)*velocity*delta
